67bf93bdd3e21252c236f9ae523c1182023b5ee9
m124578n <m23568n@gmail.com>
2023-08-14 16:41:34 +0800

    Django fetch complete

--- Django/todolist_ajax/todoapp/urls.py
+++ Django/todolist_ajax/todoapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('api/', views.api, name="api"),
     path('add/', views.add, name="add"),
     path('delete/<int:todo_id>', views.delete, name="delete"),
     path('update/<int:todo_id>', views.update, name="update"),
--- Django/todolist_ajax/todoapp/views.py
+++ Django/todolist_ajax/todoapp/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.forms.models import model_to_dict
 from .models import Todo
 import json
 
 # Create your views here.
 
 def index(request):
+    return render(request, 'base.html')
+
+
+def api(request):
     todos = Todo.objects.all()
-    return render(request, 'base.html', {"todo_list": todos})
+    return JsonResponse({"data":list(todos.values())})
 
 
 @require_http_methods(["POST"])
